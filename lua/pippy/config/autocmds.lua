80edccde837a7642fcce517532ae01217c1ab9f7
piperinnshall <piper.innshall@gmail.com>
2025-04-15 22:16:33 +1200

    feat: Add netrw plugin

--- lua/pippy/config/autocmds.lua
+++ lua/pippy/config/autocmds.lua
@@ -2,19 +2,18 @@ local api   = vim.api
 local fn    = vim.fn
 local cmd   = api.nvim_create_autocmd
 local au    = api.nvim_create_augroup
-local map   = vim.keymap.set
 
 -- Netrw
 local netrw = au('NetrwGroup', { clear = true })
 
 local last_file = ''
 
-local track_last_file = function()
+local netrw_track = function()
   local full = fn.expand('%:p')
   last_file = fn.fnamemodify(full, ':t')
 end
 
-local function focus_last_file()
+local netrw_focus = function()
   local lines = fn.getline(1, '$')
   if not vim.tbl_islist(lines) then return end
   for i, name in ipairs(lines) do
@@ -27,10 +26,11 @@ end
 
 cmd('BufEnter', {
     group       = netrw,
-    callback    = track_last_file,
+    callback    = netrw_track,
 })
 
 cmd('User', {
     group       = netrw,
-    callback    = focus_last_file,
+    pattern = "NetrwEnter",
+    callback    = netrw_focus,
 })
