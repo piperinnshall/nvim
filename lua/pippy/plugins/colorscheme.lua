c40ca7ff70824cce7f6e72f0896685f0a9a074ba
piperinnshall <piper.innshall@gmail.com>
2025-06-05 13:56:40 +1200

    chore: Transparent background

--- lua/pippy/plugins/colorscheme.lua
+++ lua/pippy/plugins/colorscheme.lua
@@ -1,9 +1,25 @@
 return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-      vim.cmd[[colorscheme tokyonight]]
-  end,
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.cmd[[
+        colorscheme tokyonight
+        hi Normal guibg=NONE ctermbg=NONE
+        hi NormalNC guibg=NONE ctermbg=NONE
+        hi SignColumn guibg=NONE ctermbg=NONE
+        hi StatusLine guibg=NONE ctermbg=NONE
+        hi StatusLineNC guibg=NONE ctermbg=NONE
+        hi VertSplit guibg=NONE ctermbg=NONE
+        hi TabLine guibg=NONE ctermbg=NONE
+        hi TabLineFill guibg=NONE ctermbg=NONE
+        hi TabLineSel guibg=NONE ctermbg=NONE
+        hi Pmenu guibg=NONE ctermbg=NONE
+        hi PmenuSel guibg=NONE ctermbg=NONE
+        hi NeoTreeNormal guibg=NONE ctermbg=NONE
+        hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
+        hi NeoTreeWinSeparator guibg=NONE ctermbg=NONE
+        ]]
+    end,
 }
