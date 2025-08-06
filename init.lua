0ecef52f96126ab752ba0232cab95406ada0ca13
piperinnshall <piper.innshall@gmail.com>
2025-08-06 19:51:23 +1200

    chore: Formatting

--- init.lua
+++ init.lua
@@ -1,31 +1,35 @@
-vim.g.maplocalleader = ' '
-vim.g.mapleader      = ' '
-vim.o.swapfile       = false
-vim.o.wrap           = false
-vim.o.number         = true
-vim.o.rnu            = true
-vim.o.expandtab      = true
-vim.o.smartindent    = true
-vim.o.hlsearch       = true
-vim.o.incsearch      = true
-vim.o.spell          = true
-vim.o.tabstop        = 2
-vim.o.shiftwidth     = 2
-vim.o.scrolloff      = 8
-vim.o.background     = 'light'
-vim.o.winborder      = 'rounded'
-vim.o.signcolumn     = 'yes'
-vim.o.fillchars      = 'eob: '
-vim.o.shell          = '/etc/profiles/per-user/piperinnshall/bin/bash'
+vim.g.maplocalleader        = ' '
+vim.g.mapleader             = ' '
+vim.g.netrw_banner          = 0
+vim.g.undotree_WindowLayout = 3
+vim.g.undotree_DiffAutoOpen = 0
+vim.g.undotree_SplitWidth   = 50
+vim.o.swapfile              = false
+vim.o.wrap                  = false
+vim.o.number                = true
+vim.o.rnu                   = true
+vim.o.expandtab             = true
+vim.o.smartindent           = true
+vim.o.hlsearch              = true
+vim.o.incsearch             = true
+vim.o.spell                 = true
+vim.o.tabstop               = 2
+vim.o.shiftwidth            = 2
+vim.o.scrolloff             = 8
+vim.o.background            = 'light'
+vim.o.winborder             = 'rounded'
+vim.o.signcolumn            = 'yes'
+vim.o.fillchars             = 'eob: '
+vim.o.shell                 = '/etc/profiles/per-user/piperinnshall/bin/bash'
 
 vim.lsp.enable({ 'c', 'lua', 'nix', 'rust', })
 
-vim.pack.add({'savq/melange-nvim'})
-vim.pack.add({'echasnovski/mini.pick'})
-vim.pack.add({'tpope/vim-surround'})
-vim.pack.add({'mbbill/undotree'})
-vim.pack.add({'itchyny/calendar.vim'})
-vim.pack.add({'mfussenegger/nvim-jdtls'})
+vim.pack.add({ 'savq/melange-nvim' })
+vim.pack.add({ 'echasnovski/mini.pick' })
+vim.pack.add({ 'tpope/vim-surround' })
+vim.pack.add({ 'mbbill/undotree' })
+vim.pack.add({ 'itchyny/calendar.vim' })
+vim.pack.add({ 'mfussenegger/nvim-jdtls' })
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>')
 vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<cr>')
@@ -41,10 +45,5 @@ vim.cmd [[ hi NormalFloat guibg=none ]]
 vim.cmd [[ hi MiniPickPrompt guibg=none ]]
 vim.cmd [[ hi MiniPickBorderText guibg=none ]]
 
-require'mini.pick'.setup({ source = { show = require'mini.pick'.default_show } })
-require'mini.pick'.setup({ window = { config = { height = math.floor(vim.o.lines / 6) } } })
-vim.g.netrw_banner   = 0
-vim.g.undotree_WindowLayout = 3
-vim.g.undotree_DiffAutoOpen = 0
-vim.g.undotree_SplitWidth   = 50
-
+require 'mini.pick'.setup({ source = { show = require 'mini.pick'.default_show } })
+require 'mini.pick'.setup({ window = { config = { height = math.floor(vim.o.lines / 6) } } })
