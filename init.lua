7135d721685682e3253ea6b072dd1e22d9a883e2
Piper Inns Hall <piper.innshall@gmail.com>
2025-10-06 16:47:31 +1300

    chore: Preserve ssh command

--- init.lua
+++ init.lua
@@ -13,8 +13,8 @@ vim.o.smartindent           = true
 vim.o.hlsearch              = true
 vim.o.incsearch             = true
 vim.o.spell                 = true
-vim.opt.exrc                = true
-vim.opt.secure              = true
+vim.o.exrc                  = true
+vim.o.secure                = true
 vim.o.tabstop               = 2
 vim.o.shiftwidth            = 2
 vim.o.scrolloff             = 8
@@ -24,6 +24,7 @@ vim.o.winborder             = 'rounded'
 vim.o.signcolumn            = 'yes'
 vim.o.fillchars             = 'eob: '
 vim.o.shell                 = '/etc/profiles/per-user/piperinnshall/bin/bash'
+vim.g.GIT_SSH_COMMAND       = "ssh -i ~/Documents/nwen243.pem"
 
 vim.lsp.enable({ 'clangd', 'lua_ls', 'nil_ls', 'rust_analyzer' })
 
@@ -32,8 +33,7 @@ vim.pack.add({ 'tpope/vim-surround' })
 vim.pack.add({ 'mbbill/undotree' })
 vim.pack.add({ 'itchyny/calendar.vim' })
 vim.pack.add({ 'mfussenegger/nvim-jdtls' })
-vim.pack.add({ 'echasnovski/mini.pick' })
-vim.pack.add({ 'echasnovski/mini.extra' })
+vim.pack.add({ '/ibhagwan/fzf-lua' })
 vim.pack.add({ 'OXY2DEV/markview.nvim' })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
@@ -42,19 +42,15 @@ vim.keymap.set('n', '<C-c>', '<cmd>cclose<cr>')
 vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end)
 vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end)
 vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<cr>')
-vim.keymap.set('n', '<C-p>', '<cmd>Pick files<cr>')
-vim.keymap.set('n', '<C-g>', '<cmd>Pick grep_live<cr>')
-vim.keymap.set('n', 'z=', require 'mini.extra'.pickers.spellsuggest)
+vim.keymap.set('n', '<C-p>', '<cmd>FzfLua files<cr>')
+vim.keymap.set('n', '<C-g>', '<cmd>FzfLua live_grep<cr>')
+vim.keymap.set('n', 'z=', '<cmd>FzfLua spell_suggest<cr>')
 
-require 'mini.pick'.setup({ window = { config = { height = math.floor(vim.o.lines / 6) } } })
+require 'fzf-lua'.setup({ 'borderless-full', winopts = { backdrop = 100 } })
 require 'markview'.setup({ preview = { hybrid_modes = { 'n', 'i' } } })
 
 vim.cmd [[ colorscheme everforest ]]
-vim.cmd [[ hi StatusLine guibg=none ]]
 vim.cmd [[ hi StatusLineNC guibg=none ]]
-vim.cmd [[ hi NormalFloat guibg=none ]]
-vim.cmd [[ hi FloatBorder guibg=none ]]
-vim.cmd [[ hi MiniPickPrompt guibg=none ]]
-vim.cmd [[ hi MiniPickBorderText guibg=none ]]
+vim.cmd [[ hi StatusLine guibg=none ]]
 
 require 'codeforces'
